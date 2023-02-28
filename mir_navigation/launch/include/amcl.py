607cefbf708fbeb9c4ef631c950601bf3faa4888
relffok <57466265+relffok@users.noreply.github.com>
2023-02-28 16:54:49 +0100

    namespace support

--- mir_navigation/launch/include/amcl.py
+++ mir_navigation/launch/include/amcl.py
@@ -56,7 +56,7 @@ def generate_launch_description():
 
     configured_params = RewrittenYaml(
         source_file=params_file,
-        root_key=namespace,
+        root_key="",
         param_rewrites=param_substitutions,
         convert_types=True)
 
